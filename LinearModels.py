1c145845bff30d877315f63a7e11f5b60be80267
Aayush0966 <aayushx699@gmail.com>
2025-09-08 04:14:52 +0545

    feat: add regularization support to Linear and Logistic regression models

--- LinearModels.py
+++ LinearModels.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class LinearRegression():
-    def best_fit(self, x, y):
+    def best_fit(self, x, y, rg):
         X = np.array(x)
         Y = np.array(y)
         
@@ -20,16 +20,17 @@ class LinearRegression():
         x_std[x_std == 0] = tolerance  # in case std is 0
         x_scaled = (X - x_mean) / x_std  # scaling because some features are large in size and some are small
         
-        w_scaled = np.zeros(x_scaled.shape[1])
+        w_scaled = np.zeros((x_scaled.shape[1], ))  
         b_scaled = 0
         lr = 0.01
+        alpha=0.01
         epochs = 100000
         
         for i in range(epochs):
-            dw, db = self.find_slope(w_scaled, b_scaled, x_scaled, Y)
+            dw, db = self.find_slope(w_scaled, b_scaled, x_scaled, Y, alpha, rg)
             w_scaled = w_scaled - lr * dw
             b_scaled = b_scaled - lr * db
-            current_cost = self.cost_function(w_scaled, b_scaled, x_scaled, Y)
+            current_cost = self.cost_function(w_scaled, b_scaled, x_scaled, Y, alpha, rg)
 
             if previous_cost != float('inf'):
                 if abs(previous_cost - current_cost) / previous_cost < 1e-6:
@@ -48,25 +49,39 @@ class LinearRegression():
             b_original = 0
 
         # Calculate final cost with original weights
-        final_cost = self.cost_function(w_original, b_original, X, Y)
+        final_cost = self.cost_function(w_original, b_original, X, Y, alpha, "")
         if np.isclose(final_cost, 0):
             final_cost = 0
 
         
         return w_original, b_original, final_cost
 
-    def find_slope(self, w, b, x, y):
-        n = len(y)
-        predictions =  x.dot(w) + b
-        dw = - (1/n)  * x.T.dot(y - predictions)
-        db = - np.mean(y - predictions)
-        return dw, db
-    
+    def find_slope(self, w, b, x, y, alpha, rg):
+        n = len(x)
+        prediction = self.predict(w, x ,b)
 
-    def cost_function(self, w, b, x, y):
-        n = len(y)
-        predictions = x.dot(w) + b
-        return (1/(2*n)) * np.sum((y - predictions) ** 2)
+        if rg == "l1":
+            dw = -(1 / n) * x.T.dot(y - prediction) + alpha * np.sign(w)
+        elif rg == "l2":
+            dw = -(1 / n) * x.T.dot(y - prediction) + 2 * alpha * w
+        else:
+            dw = -(1 / n) * x.T.dot(y - prediction)
+
+        db = -np.mean(y - prediction)
+        return dw, db
+
+    def cost_function(self, w, b, x, y, alpha, rg):
+        y_predictions = self.predict(w, x, b)
+        mse = np.mean((y - y_predictions) ** 2)
+
+        if rg == "l1":
+            regularization_penalty = alpha * np.sum(np.abs(w))
+        elif rg == "l2":
+            regularization_penalty = alpha * np.sum(w**2)
+        else:
+            regularization_penalty = 0
+
+        return mse + regularization_penalty
     
     def predict(self, w, x, b):
         return x.dot(w) + b
@@ -74,7 +89,7 @@ class LinearRegression():
 
 
 class LogisticRegression():
-    def best_fit(self, x, y):
+    def best_fit(self, x, y, rg):
         X = np.array(x)
         Y = np.array(y)
         
@@ -96,12 +111,13 @@ class LogisticRegression():
         b_scaled = 0
         lr = 0.01
         epochs = 10000
+        alpha=0.01
         
         for i in range(epochs):
-            dw, db = self.find_slope(w_scaled, b_scaled, x_scaled, y)
+            dw, db = self.find_slope(w_scaled, b_scaled, x_scaled, y, alpha, rg)
             w_scaled = w_scaled - lr * dw
             b_scaled = b_scaled - lr * db
-            current_cost = self.cost_function(w_scaled,b_scaled,x_scaled,y)
+            current_cost = self.cost_function(w_scaled,b_scaled,x_scaled,y, alpha, rg)
             
             if previous_cost != float("inf"):
                 if abs(previous_cost - current_cost) / previous_cost < 1e-6:
@@ -114,27 +130,43 @@ class LogisticRegression():
         b_orig = b_scaled - np.sum(w_orig * x_mean)
         
         w_orig = np.where(np.isclose(w_orig, 0), 0, w_orig)
-        b_orig = np.where(np.isclose(b_orig, 0), 0, b_orig)
+        if np.isclose(b_orig, 0):
+            b_orig = 0
         
-        final_cost = self.cost_function(w_orig, b_orig, X, Y )
-        final_cost = np.where(np.isclose(final_cost,0 ), 0, final_cost)
+        final_cost = self.cost_function(w_orig, b_orig, X, Y, alpha, "")
+        if np.isclose(final_cost, 0):
+            final_cost = 0
         
         return w_orig, b_orig, final_cost
                     
     
-        
-    def cost_function(self, w, b, x, y):
+    def cost_function(self, w, b, x, y, alpha=0, rg=None):  
         prediction = self.predict(w, x, b)
-        return - (np.mean(y * np.log(prediction) + (1-y) * np.log(1 - prediction)))
+        prediction = np.clip(prediction, 1e-15, 1 - 1e-15)
+        cross_entropy = - (np.mean(y * np.log(prediction) + (1-y) * np.log(1 - prediction)))
         
+        if rg == "l1":
+            regularization_penalty = alpha * np.sum(np.abs(w))
+        elif rg == "l2":
+            regularization_penalty = alpha * np.sum(w**2)
+        else:
+            regularization_penalty = 0
         
-    
-    def find_slope(self, w, b, x, y):
-        prediction  = self.predict(w,x, b)
-        dw =  (1/len(y)  )*  x.T.dot(prediction - y)
+        return cross_entropy + regularization_penalty
+        
+    def find_slope(self, w, b, x, y, alpha=0, rg=None):  
+        prediction = self.predict(w, x, b)
+        dw_base = (1/len(y)) * x.T.dot(prediction - y)
+        
+        if rg == "l1":
+            dw = dw_base + alpha * np.sign(w)
+        elif rg == "l2":
+            dw = dw_base + 2 * alpha * w
+        else:
+            dw = dw_base
+        
         db = np.mean(prediction - y)
         return dw, db
-        
     
     def predict(self, w, x, b):
         z = b + np.dot(x,w )
--- LinearRegression.py
+++ LinearRegression.py
@@ -4,39 +4,65 @@ import matplotlib.pyplot as plt
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([2, 4, 6, 8, 10])
 
-#model for computing 
+
+# model for computing
 def compute_model(x, w, b):
     return w * x + b
 
-#MSE 
-def model_error(x, y, w, b):
-    n = len(x)
-    y_predictions = compute_model(x, w, b)
-    return 1 / (2 * n) * np.sum((y - y_predictions) ** 2)  
 
-#provides the derivative w.r.t w and b
-def gradient_descent(x, y, w, b):   
+def model_error(x, y, w, b, alpha=0, rg=None):
+    y_predictions = compute_model(x, w, b)
+    mse = np.mean((y - y_predictions) ** 2)
+
+    if rg == "l1":
+        regularization_penalty = alpha * np.sum(np.abs(w))
+    elif rg == "l2":
+        regularization_penalty = alpha * np.sum(w**2)
+    else:
+        regularization_penalty = 0
+
+    return mse + regularization_penalty
+
+
+def gradient_descent(x, y, w, b, alpha, rg):
     n = len(x)
     prediction = compute_model(x, w, b)
-    dw = -(1 / n) * x.T.dot(y - prediction)
-    db = - np.mean(y - prediction)
+
+    if rg == "l1":
+        dw = -(1 / n) * x.T.dot(y - prediction) + alpha * np.sign(w)
+    elif rg == "l2":
+        dw = -(1 / n) * x.T.dot(y - prediction) + 2 * alpha * w
+    else:
+        dw = -(1 / n) * x.T.dot(y - prediction)
+
+    db = -np.mean(y - prediction)
     return dw, db
 
-#updates the w and b
+
 def update_parameters(w, b, dw, db, lr):
     w = w - lr * dw
     b = b - lr * db
     return w, b
 
-lr = 0.01 #learning rate 
-epochs = 100000 #iteration
-w, b = 0.0, 0.0 
+
+# updates the w and b
+def update_parameters(w, b, dw, db, lr):
+    w = w - lr * dw
+    b = b - lr * db
+    return w, b
+
+
+lr = 0.01  # learning rate
+epochs = 100000  # iteration
+w, b = 0.0, 0.0
+alpha= 0.1
+rg=""
 
 for i in range(epochs):
-    dw, db = gradient_descent(x, y, w, b)
+    dw, db = gradient_descent(x, y, w, b, alpha, rg)
     w, b = update_parameters(w, b, dw, db, lr)
     if i % 100 == 0:
-        cost = model_error(x, y, w, b)
+        cost = model_error(x, y, w, b, alpha, rg)
 
 y_predictions = compute_model(x, w, b)
 error = model_error(x, y, w, b)
